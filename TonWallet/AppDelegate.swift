b1e2c391838de259c0f132f9918bb593fcb16f11
Vladislav Kiryukhin <vkiryukhin@yandex-team.ru>
2023-03-24 16:30:14 +0800

    Initial Commit

---
+++ TonWallet/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TonWallet
+//
+//  Created by Vladislav Kiriukhin on 24.03.2023.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
